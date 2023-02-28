eaaa08eba67d3e43db20094c066ad6d39bd6b6b9
David <davidlmwangi7@gmail.com>
2023-02-28 20:28:15 +0300

    Add 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @Parent: The parent of the node
+ * @value: Value of the node
+ *
+ * Return: A pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = calloc(1, sizeof(binary_tree_t));
+	if (!root)
+		return (NULL);
+	node->parent = parent;
+	node->n = value;
+
+	return (node);
+}
