e332e376353342db6e0b5ab7c8b81abf3092af79
David <davidlmwangi7@gmail.com>
2023-02-28 22:53:52 +0300

    hed

--- binary_trees.h
+++ binary_trees.h
@@ -6,6 +6,8 @@
 #include <stddef.h>
 #include <string.h>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -34,5 +36,6 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
