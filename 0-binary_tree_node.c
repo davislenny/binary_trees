37b9a56b2c539ee41c1eead57f4c837f63109ac1
David <davidlmwangi7@gmail.com>
2023-02-28 20:52:53 +0300

    Add 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL;
 
 	node = calloc(1, sizeof(binary_tree_t));
-	if (!root)
+	if (!node)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
