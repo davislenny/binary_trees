be36e3f175ad462f7d4d3db56e819caeb7747fde
David <davidlmwangi7@gmail.com>
2023-02-28 20:32:03 +0300

    Add header

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
