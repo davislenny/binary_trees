851bed17058719e9afb7dde04e7b1c6679302845
David <davidlmwangi7@gmail.com>
2023-03-01 20:01:22 +0300

    Add 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - Measures the size of a binary tree
  * @tree: Points to root node of the tree to measure the size
- * 
+ *
  * Return: 0 if tree=NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
